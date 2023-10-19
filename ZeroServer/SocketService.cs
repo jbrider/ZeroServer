fba71fd3854557d2cd238d2f6e5ff7b0ed815da1
Jason Brider <jason.brider@gmail.com>
2023-10-19 11:45:18 +1000

    Add project files.

---
+++ ZeroServer/SocketService.cs
@@ -0,0 +1,36 @@
+﻿
+using Microsoft.AspNetCore.Hosting.Server;
+using NetMQ;
+using NetMQ.Sockets;
+
+namespace ZeroServer
+{
+    public class SocketService : BackgroundService
+    {
+        
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            Console.WriteLine("Server starting...");
+
+            await Task.Run(() =>
+            {
+                using (var server = new ResponseSocket())
+                {
+                    server.Bind("tcp://*:5555");
+                    while (!stoppingToken.IsCancellationRequested)
+                    //while (true)
+                    {
+                        //_logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                        var message = server.ReceiveFrameString();
+                        Console.WriteLine("Received {0}", message);
+                        // processing the request
+                        Console.WriteLine("Sending World");
+                        server.SendFrame("World");
+                    }
+                }
+            });
+            Console.WriteLine("Server started...");
+        }
+
+    }
+}
