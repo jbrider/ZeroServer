aa3b31619276e5db65afbde220981b02a9b496da
Jason Brider <jason.brider@gmail.com>
2023-10-20 15:32:27 +1000

    Getting push/pull sockets working

---
+++ ZeroServer/JobsService.cs
@@ -0,0 +1,35 @@
+﻿using NetMQ;
+using NetMQ.Sockets;
+
+namespace ZeroServer;
+
+public class JobsService
+{
+    //collate jobs and ensure they're all complete
+    //add a timeout to resend any incomplete jobs
+    public async Task Start(int jobsNumber)
+    {
+        await Task.Run(() =>
+        {
+            using (var receiver = new PullSocket("@tcp://*:5558"))
+            {
+                for (int taskNumber = 0; taskNumber < 100; taskNumber++)
+                {
+                    var workerDoneTrigger = receiver.ReceiveFrameString();
+                    if (taskNumber % 10 == 0)
+                    {
+                        Console.Write(":");
+                    }
+                    else
+                    {
+                        Console.Write(".");
+                    }
+                }
+                //Calculate and report duration of batch
+                Console.WriteLine("Jobs Complete.");
+                Console.WriteLine();
+
+            }
+        });
+    }
+}
--- ZeroServer/SocketService.cs
+++ ZeroServer/SocketService.cs
@@ -2,30 +2,58 @@
 using Microsoft.AspNetCore.Hosting.Server;
 using NetMQ;
 using NetMQ.Sockets;
+using System.Reflection;
+using static System.Reflection.Metadata.BlobBuilder;
 
 namespace ZeroServer
 {
     public class SocketService : BackgroundService
     {
-        
+        private JobsService _jobs;
+
+        public SocketService(JobsService jobs)
+        {
+            _jobs = jobs;
+        }
+
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             Console.WriteLine("Server starting...");
 
             await Task.Run(() =>
             {
+               
+                using (var queue = new PushSocket("@tcp://*:5557"))
                 using (var server = new ResponseSocket())
                 {
                     server.Bind("tcp://*:5555");
                     while (!stoppingToken.IsCancellationRequested)
-                    //while (true)
                     {
                         //_logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                        var message = server.ReceiveFrameString();
-                        Console.WriteLine("Received {0}", message);
+                        var jobsMsg = server.ReceiveFrameString();
+                        int jobsCount = Convert.ToInt32(jobsMsg);
+                        Console.WriteLine("Received {0}", jobsCount);
+                        _jobs.Start(jobsCount);
                         // processing the request
+                        Console.WriteLine("Sending tasks to workers");
+                        //initialise random number generator
+                        Random rand = new Random(0);
+                        //expected costs in Ms
+                        int totalMs = 0;
+                        //send 100 tasks (workload for tasks, is just some random sleep time that
+                        //the workers can perform, in real life each work would do more than sleep
+                        for (int taskNumber = 0; taskNumber < jobsCount; taskNumber++)
+                        {
+                            //Random workload from 1 to 100 msec
+                            int workload = rand.Next(0, 100);
+                            totalMs += workload;
+                            Console.WriteLine("Workload : {0}", workload);
+                            queue.SendFrame(workload.ToString());
+                        }
+                        Console.WriteLine("Total expected cost : {0} msec", totalMs);
+
                         Console.WriteLine("Sending World");
-                        server.SendFrame("World");
+                        server.SendFrame(totalMs.ToString());
                     }
                 }
             });
